dce38be6f754d4a64df592d971ea9791133d6c59
Mohit-R-04 <mohit2310893@ssn.edu.in>
2025-06-04 08:56:38 +0530

    made as maven project

---
+++ spring-boot-backend/src/main/java/in/edu/ssn/hostel/HostelAllotmentApplication.java
@@ -0,0 +1,11 @@
+package in.edu.ssn.hostel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelAllotmentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HostelAllotmentApplication.class, args);
+    }
+}
