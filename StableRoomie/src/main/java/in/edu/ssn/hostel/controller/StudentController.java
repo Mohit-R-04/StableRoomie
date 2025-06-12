58d5a95690620c951614d0d9aefb6122ae6d41c5
Mohit-R-04 <mohit2310893@ssn.edu.in>
2025-06-12 21:10:56 +0530

    fix errors

--- StableRoomie/src/main/java/in/edu/ssn/hostel/controller/StudentController.java
+++ StableRoomie/src/main/java/in/edu/ssn/hostel/controller/StudentController.java
@@ -1,11 +1,15 @@
 package in.edu.ssn.hostel.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import in.edu.ssn.hostel.model.Student;
 import in.edu.ssn.hostel.service.studentService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 public class StudentController {
