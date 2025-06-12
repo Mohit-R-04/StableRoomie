de9114865b7457729372d96285cf51cfe8eb9762
Priyan-glitch <priyanrr2005@gmail.com>
2025-06-12 19:56:04 +0530

    pom file and new pakages

--- StableRoomie/src/main/java/in/edu/ssn/hostel/HostelAllotmentApplication.java
+++ StableRoomie/src/main/java/in/edu/ssn/hostel/HostelAllotmentApplication.java
@@ -2,8 +2,10 @@ package in.edu.ssn.hostel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+
 public class HostelAllotmentApplication {
     public static void main(String[] args) {
         SpringApplication.run(HostelAllotmentApplication.class, args);
---
+++ StableRoomie/src/main/java/in/edu/ssn/hostel/service/studentService.java
@@ -0,0 +1,20 @@
+package in.edu.ssn.hostel.service;
+
+import in.edu.ssn.hostel.model.Student;
+import in.edu.ssn.hostel.repo.studentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class studentService {
+    @Autowired
+    studentRepo repo;
+    public void addStudent(Student stud){
+        repo.save(stud);
+    }
+    public Student getStudent(Integer studId){
+        return repo.findById(studId).orElse(new Student(100, "None", 0));
+    }
+}
